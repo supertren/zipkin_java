e9e7f2a60d9b641473dd27fa6c9e610d97efacbc
supertren <43068656+supertren@users.noreply.github.com>
2025-02-27 01:13:24 +0100

    Create ServiceBController.java

---
+++ src/ServiceBController.java
@@ -0,0 +1,14 @@
+package com.example.serviceb;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/service-b")
+public class ServiceBController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from Service B";
+    }
+}
